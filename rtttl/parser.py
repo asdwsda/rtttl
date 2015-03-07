6435f73cc401ae5688d73a7dd235603749aa9a12
Peter Szabo <asdwsda@gmail.com>
2015-03-07 15:30:08 +0100

    more verbose error handling

--- rtttl/parser.py
+++ rtttl/parser.py
@@ -1,5 +1,5 @@
 import re
-from rtttl.exceptions import InvalidDefaults, InvalidNote, InvalidElement, InvalidRTTTLFormat
+from rtttl.exceptions import InvalidDefaults, InvalidElement, InvalidNote, InvalidRTTTLFormat
 
 
 def remove_whitespaces(string):
@@ -10,7 +10,11 @@ def parse_rtttl(rtttl_str, strict_note_syntax=False):
     rtttl_parts = rtttl_str.split(':')
 
     if len(rtttl_parts) != 3:
-        raise InvalidRTTTLFormat()
+        raise InvalidRTTTLFormat('RTTTL string should contain title, defaults and data part, separated by colon')
+
+    if len(rtttl_parts[2]) == 0:
+        raise InvalidRTTTLFormat('Data part cannot be empty')
+
 
     defaults = parse_defaults(remove_whitespaces(rtttl_parts[1]))
     parsed_notes = parse_data(remove_whitespaces(rtttl_parts[2]).lower(), strict_note_syntax)
@@ -33,9 +37,9 @@ def parse_defaults(defaults_str):
                 'bpm': parse_bpm(defaults['b'])
                 }
         else:
-            raise InvalidDefaults()
-    except:
-        raise InvalidDefaults()
+            raise InvalidDefaults('Invalid defaults section format')
+    except InvalidElement as element:
+        raise InvalidDefaults('%s in %s' % (element.message, defaults_str))
 
     return parsed_defaults
 
@@ -58,8 +62,10 @@ def parse_note(note_str):
         elements = re.findall(r'^(\d{0,2})([pbeh]|[cdfga]#?)(\d?)(\.?)$', note_str)[0]
         funcs = (parse_duration, parse_pitch, parse_octave, has_dot)
         elements = [func(element) for func, element in zip(funcs, elements)]
-    except:
-        raise InvalidNote()
+    except InvalidElement as element:
+        raise InvalidNote(' %s in note %s' % (element.message, note_str))
+    except IndexError:
+        raise InvalidNote('Invalid note: %s' % note_str)
     keys = ('duration', 'pitch', 'octave', 'dot')
     return dict(zip(keys, elements))
 
@@ -97,7 +103,7 @@ def parse_element(element, allowed):
     if element in allowed:
         return element
     else:
-        raise InvalidElement()
+        raise InvalidElement('Element \'%s\' is not allowed' % element)
 
 
 def has_dot(dot):
