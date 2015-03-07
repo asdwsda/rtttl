7f017193965ef05515732ef0906844234bc419e8
Peter Szabo <asdwsda@gmail.com>
2015-03-07 18:08:35 +0100

    package infos

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='rtttl',
+    packages=['rtttl'],
+    version='0.2',
+    description='Nokia Ring Tone Text Transfer Language (RTTTL) parser for python',
+    license='GNU General Public License v3',
+    author='Peter Szabo',
+    author_email='asdwsda@gmail.com',
+    url='https://github.com/asdwsda/rtttl',
+    download_url='https://github.com/asdwsda/rtttl/tarball/0.2',
+    keywords = ['rtttl', 'ringtone', 'nokia'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Sound/Audio',
+        ]
+)
